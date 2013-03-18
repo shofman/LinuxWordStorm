625ef0f128b19924e0790867f1ad5b26ff24d150
Scott Hofman <s0943941@sms.ed.ac.uk>
2013-03-18 02:09:57 +0000

    Have the basic ability to view an entire wordstorm for a single user. Refactoring and adding ability to view all storms and have this integrated into profile page

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,8 +1,9 @@
 class GalleryController < ApplicationController
+  respond_to :html, :xml, :json
   def view
 	if (params.has_key?(:wordstorm))
 		@storm = WordStorm.find(Integer(params[:stormid]))
-		@params = params
+		@curr_storm_id = params[:stormid]
 		#Update storm and image positions based on create
 		@storm.name = params[:wordstorm][:name]
 		@storm.images.each_with_index do |image, i|
@@ -13,13 +14,40 @@ class GalleryController < ApplicationController
 			end
 		end
 		@storm.save
-	else 
+	elsif (params.has_key?(:back))
+		if !params[:stormid].nil?
+			@stormid = Integer(params[:stormid])-1
+			if @stormid < WordStorm.first.id
+				@stormid = WordStorm.last.id
+			end
+			@storm = WordStorm.find(@stormid)
+		else
+			@storm = WordStorm.last
+			flash[:notice] = "There's an error in the database"
+		end
+		@curr_storm_id = @storm.id
+	elsif (params.has_key?(:front))
+		if !params[:stormid].nil?
+			@stormid = Integer(params[:stormid])+1
+			if @stormid > WordStorm.last.id
+				@stormid = WordStorm.first.id
+			end
+			@storm = WordStorm.find(@stormid)
+		else
+			@storm = WordStorm.first
+			flash[:notice] = "There's an error in the database"
+		end
+		@curr_storm_id = @storm.id
+	else
 		#Viewing as normal
 		#TODO make this generic so users don't have to login
 		@user = User.find(session[:user_id])
-		@storm = @user.word_storms.first
+		@storm = @user.word_storms.last
+		@curr_storm_id = @storm.id
 	end
 	@files = Dir.glob(@storm.file_location + '/*.png')
+	
+	#render :json => @storm
   end
 
   def edit
---
+++ lib/gallery.rb
@@ -0,0 +1,3 @@
+module Gallery
+
+end
